d18d9dff8fea7a0dbe176b844bbea8ecad92749c
asfahan321 <muhammadasfahan689@gmail.com>
2025-02-17 21:19:51 +0500

    ..

--- work/lib/Lp.dart
+++ work/lib/Lp.dart
@@ -148,7 +148,7 @@ class _LpState extends State<Lp> {
                       // Navigate to the Home page
                       Navigator.push(
                         context,
-                        MaterialPageRoute(builder: (context) => const KarachiPage()),
+                        MaterialPageRoute(builder: (context) => const Home()),
                       );
                     },
                     style: TextButton.styleFrom(
